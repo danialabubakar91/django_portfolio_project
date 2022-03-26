da610e537ae0c11144f56e03ba5a45ad9cfd6e39
Danial <danialabubakar91@gmail.com>
2022-03-26 11:58:36 +0800

    bootstrap navbar styling checkpoint 1

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Blog
+from django.urls import resolve
 
 # Create your views here.
 def all_blogs(request):
+    current_url = resolve(request.path_info).url_name
     blogs = Blog.objects.order_by('-date')
-    return render(request, 'blog/all_blogs.html',{'blogs':blogs})
+    return render(request, 'blog/all_blogs.html',{'blogs':blogs,'current_url':current_url})
 
 def detail(request,blog_id):
+    current_url = resolve(request.path_info).url_name
     blog = get_object_or_404(Blog,pk=blog_id)
-    return render(request, 'blog/detail.html',{'blog':blog})
+    return render(request, 'blog/detail.html',{'blog':blog,'current_url':current_url})
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
+from django.urls import resolve
+
 
 # Create your views here.
 def home(request):
+    current_url = resolve(request.path_info).url_name
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects':projects})
+    return render(request, 'portfolio/home.html', {'projects':projects, 'current_url':current_url})
