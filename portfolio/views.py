189c35cd62d960d4ed1003173590104dbcd411e9
Danial <danialabubakar91@gmail.com>
2022-04-11 17:14:56 +0800

    Completed styling

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,4 +8,9 @@ from django.urls import resolve
 def home(request):
     current_url = resolve(request.path_info).url_name
     projects = Project.objects.all()
+    # project_description = projects[0].description.replace("\\n", "<br>")
+
+    # for idx, item in enumerate(projects):
+    #     projects[idx].description = projects[idx].description.replace("\\n", "<br>")
+    
     return render(request, 'portfolio/home.html', {'projects':projects, 'current_url':current_url})
